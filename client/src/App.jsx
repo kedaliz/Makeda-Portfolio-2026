7f59ef46fe989baca4f12c654c1da97e893f5bfe
makeda-elizabeth cabey <mlc359@cornell.edu>
2026-04-09 23:44:30 +0000

    removed '

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -358,7 +358,7 @@ function App() {
                 Explore my work and see what I built
               </p>
               <p className="internship-cta">
-                Product Design & Tech Policy Intern | Open to Summer/Fall 2026 opportunities in UX/UI and Library/Archival Science."
+                Product Design & Tech Policy Intern | Open to Summer/Fall 2026 opportunities in UX/UI and Library/Archival Science.
               </p>
             </section>
 
